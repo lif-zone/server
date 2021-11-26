716e3af219a2a22a1af47138f8e3b450bd23f884
lif-rnd <lif.zone.main@gmail.com>
2021-11-26 08:21:24 +0200

    simple http

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict'; /*jslint node:true*/
 import dnss from './lib/dnss.js';
+import https from './lib/https.js';
 
 function start(){
   console.log('lif server start');
   dnss.start(); // XXX: need dnss.close()
+  https.start(); // XXX: need dnss.close()
 }
 
 start();
