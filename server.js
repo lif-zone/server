f2c8855d66aed08e9cd253f5d46af31063c151a6
lif-rnd <lif.zone.main@gmail.com>
2021-11-27 11:36:27 +0200

    support external ddns

--- server.js
+++ server.js
@@ -2,10 +2,18 @@
 import dnss from './lib/dnss.js';
 import https from './lib/https.js';
 
+let DEV = true;
+const conf = DEV ? {
+  dnss: {port: 53, ip: '127.0.0.1', dns: '8.8.8.8', domain: 'poc.lif.zone'}
+} : {
+  dnss: {port: 53, ip: '3.12.37.122', dns: '8.8.8.8', domain: 'poc.lif.zone'}
+};
+
 function start(){
   console.log('lif server start');
-  dnss.start(); // XXX: need dnss.close()
-  https.start(); // XXX: need dnss.close()
+  // XXX: split into seperate process
+  dnss.start(conf.dnss); // XXX: need dnss.close()
+  https.start(); // XXX: need https.close()
 }
 
 start();
