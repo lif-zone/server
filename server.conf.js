a887d9afdbfc7bafc0eb8dc33297607743d45a87
lif-rnd <lif.zone.main@gmail.com>
2021-11-29 11:37:25 +0200

    ssl support

--- server.conf.js
+++ server.conf.js
@@ -2,7 +2,11 @@
 const env = process.env, DEV = env.DEV;
 const E = {
   dnss: {port: 53, ip: DEV ? '127.0.0.1' : '3.12.37.122', dns: '8.8.8.8',
-    domain: 'poc.lif.zone'}
+    domain: 'poc.lif.zone'
+  }, http_server: {
+    ssl: {key: '/var/lif/ssl/STAR_lif_zone.key',
+      cert: '/var/lif/ssl/STAR_lif_zone.crt'}
+  },
 };
 
 export default E;
