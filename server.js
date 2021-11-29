5ab2069d3212ea084d974ebec8692d7837f41134
lif-rnd <lif.zone.main@gmail.com>
2021-11-29 12:03:25 +0200

    rename https.js -> https_server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict'; /*jslint node:true*/
 import dns_server from './lib/dns_server.js';
-import https from './lib/https.js'; // XXX: rename httpd.js
+import https_server from './lib/https_server.js'; // XXX: rename httpd.js
 
 function start(){
   console.log('lif server start');
   // XXX: split into seperate process
   dns_server.start(); // XXX: need dns_server.close()
-  https.start(); // XXX: need https.close()
+  https_server.start(); // XXX: need https_server.close()
 }
 
 start();
