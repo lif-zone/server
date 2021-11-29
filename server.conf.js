e24039a1b371f9f05ce53829e9c6bc3ad675fa53
lif-rnd <lif.zone.main@gmail.com>
2021-11-29 12:01:28 +0200

    rename dnss.js -> dns_server + coding fixes

--- server.conf.js
+++ server.conf.js
@@ -1,7 +1,7 @@
 'use strict'; /*jslint node:true*/
 const env = process.env, DEV = env.DEV;
 const E = {
-  dnss: {port: 53, ip: DEV ? '127.0.0.1' : '3.12.37.122', dns: '8.8.8.8',
+  dns_server: {port: 53, ip: DEV ? '127.0.0.1' : '3.12.37.122', dns: '8.8.8.8',
     domain: 'poc.lif.zone'
   }, http_server: {
     ssl: {key: '/var/lif/ssl/STAR_lif_zone.key',
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 'use strict'; /*jslint node:true*/
-import conf from './server.conf.js';
-import dnss from './lib/dnss.js';
+import dns_server from './lib/dns_server.js';
 import https from './lib/https.js'; // XXX: rename httpd.js
 
 function start(){
   console.log('lif server start');
   // XXX: split into seperate process
-  dnss.start(conf.dnss); // XXX: need dnss.close()
+  dns_server.start(); // XXX: need dns_server.close()
   https.start(); // XXX: need https.close()
 }
 
