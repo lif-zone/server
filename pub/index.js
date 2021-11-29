4143b3b274f5e7596ebc195a8d9fd0b40abdbdf0
lif-rnd <lif.zone.main@gmail.com>
2021-11-29 17:19:58 +0200

    coding

--- pub/index.js
+++ pub/index.js
@@ -2,14 +2,10 @@
 var ws_client = require('../lib/ws_client.js');
 
 function test_signalhub(){
-  console.log('xxx hello %o %o', require, ws_client);
   var ws = ws_client(['ws://poc.lif.zone:3030']);
-  ws.subscribe('my-channel').on('data', message=>
+  ws.subscribe('my_channel').on('data', message=>
     console.log('new message received', message));
-  ws.on('open', ()=>{
-    console.log('XXX broadcast');
-    ws.broadcast('my-channel', {hello: 'world '+Date.now()});
-  });
+  ws.on('open', ()=>ws.broadcast('my_channel', {hello: 'world '+Date.now()}));
 }
 
 test_signalhub();
