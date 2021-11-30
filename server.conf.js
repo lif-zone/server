d6a3b65b19e297054ef5c2ab53a72de897b9fdb5
lif-rnd <lif.zone.main@gmail.com>
2021-11-30 10:15:43 +0200

    fix log prints

--- server.conf.js
+++ server.conf.js
@@ -3,7 +3,7 @@ const env = process.env, DEV = env.DEV;
 const E = {
   dns_server: {port: 53, ip: DEV ? '127.0.0.1' : '3.12.37.122', dns: '8.8.8.8',
     domain: 'poc.lif.zone'
-  }, http_server: {
+  }, http_server: {http_port: 80, https_port: 443,
     ssl: {key: '/var/lif/ssl/STAR_lif_zone.key',
       cert: '/var/lif/ssl/STAR_lif_zone.crt'}
   }, ws_server: {ws_port: 3030, wss_port: 3031,
