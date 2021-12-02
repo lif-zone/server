7e146ea14997829db0e6f10cd287e78351654b6f
lif-rnd <lif.zone.main@gmail.com>
2021-12-02 13:46:01 +0200

    revert back to ws

--- pub/index.js
+++ pub/index.js
@@ -1,51 +1,19 @@
 // XXX: replace require with import
-const SimplePeerWrapper = require('simple-peer-wrapper');
-
-function connect(){
-  const options = {
-    debug: true,
-    serverUrl: 'wss://poc.lif.zone:3031',
-    simplePeerOptions: {},
-  };
-  const spw = new SimplePeerWrapper(options);
-  let socket = spw.socketClient.socket;
-  // socket
-  socket.on('connect', function(){
-    console.log('socket: connect %s', this.io.uri); });
-  socket.on('connect_error', o=>console.log('socket: connect_error %o', o));
-  socket.on('connect_timeout', ()=>console.log('socket: connect_timeout'));
-  socket.on('reconnect', ()=>console.log('socket: reconnect'));
-  socket.on('reconnecting', ()=>console.log('socket: reconnecting'));
-  socket.on('reconnect_failed', ()=>console.log('socket: reconnect_failed'));
-  // signaling
-  socket.on('created', room=>console.log('signal: created room %s', room));
-  socket.on('full', room=>console.log('signal: full room %s', room));
-  socket.on('join', room=>console.log('signal: join room %s', room));
-  socket.on('joined', room=>console.log('signal: joined room %s', room));
-  socket.on('initiate peer', function(room){
-    console.log('signal: XXX initiate peer id %s room %s', this.id, room);
+const ws_client = require('../lib/ws_client.js');
+const wsc = ws_client(['wss://poc.lif.zone:3031']);
+function test_signalhub(){
+  var messages = [];
+  wsc.subscribe('my_channel').on('data', msg=>{
+    console.log('got msg', msg);
+    messages.push(JSON.stringify(msg));
+    document.querySelector('#ws_incoming').innerText = messages.join('\n');
   });
-  socket.on('sending signal',
-    message=>console.log('signal: sending signal room %o', message));
-  socket.on('log', array=>console.log('signal: log %o', array));
-  socket.on('message', array=>console.log('signal: message %o', array));
-  spw.connect();
-  spw.on('error', data=>console.log('spw: error %o', data));
-  spw.on('close', data=>console.log('spw: close %o', data));
-  spw.on('data', data=>console.log('spw: data %o', data));
-  // XXX: need proper handling of unload
-  window.onbeforeunload = ()=>spw.close();
-  window.webrtc_test_send = ()=>{
-    let msg = document.querySelector('#msg').value;
-    console.log('XXX send webrtc_test_send %s', msg);
-    spw.send({ts: Date.now(), msg});
-  };
-  window.ws_test_send = ()=>{
-    let msg = document.querySelector('#msg').value;
-    console.log('XXX send ws_test_send %s', msg);
-    socket.send({ts: Date.now(), msg});
-  };
 }
+
+window.ws_test_send = function ws_test_send(){
+  let msg = document.querySelector('#ws_msg').value;
+  wsc.broadcast('my_channel', {ts: +Date.now(), msg});
+};
 
 function init(){
   if (location.pathname=='/' &&
@@ -55,17 +23,14 @@ function init(){
       <div>
         <div><b>LIF</b></div>
         <div>
-          <input id=msg value=Message>
+          <input id=ws_msg value=Message>
           <input type=button value=Broadcast onClick="ws_test_send()">
-          <input type=button value=Broadcast-ws onClick="ws_test_send()">
-          <input type=button value=Broadcast-webrtc
-            onClick="webrtc_test_send()">
         </div>
         <pre id=ws_incoming>
         <pre>
       </div>
     `;
-    connect();
+    test_signalhub();
   }
   else if (window.self!==window.top)
     document.body.innerHTML = 'iframe for '+location.href;
@@ -77,3 +42,4 @@ function init(){
 }
 
 init();
+
--- server.js
+++ server.js
@@ -2,15 +2,13 @@
 import dns_server from './lib/dns_server.js';
 import https_server from './lib/https_server.js';
 import ws_server from './lib/ws_server.js';
-import signal_server from './lib/signal_server.js';
 
 function init(){
   console.log('lif server start');
   // XXX: split into seperate process
   dns_server.start(); // XXX: need dns_server.stop()
   https_server.start(); // XXX: need https_server.stop()
-  if (0) ws_server.start(); // XXX: need ws_server.stop()
-  signal_server.start(); // XXX: need signal_server.stop()
+  ws_server.start(); // XXX: need ws_server.stop()
 }
 
 init();
