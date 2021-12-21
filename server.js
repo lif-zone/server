4a3b0d59f4c195852df040be0c8fc3fc6bf42768
lif-rnd <lif.zone.main@gmail.com>
2021-12-21 15:17:51 +0200

    rm obsolete turn server

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import nconf from 'nconf';
 import util from './util/util.js';
 import dns_server from './lib/dns_server.js';
 import https_server from './lib/https_server.js';
-import turn_server from './lib/turn_server.js';
 import peer_relay from './peer-relay/client.js';
 import crypto from 'crypto';
 
@@ -22,7 +21,6 @@ function init(){
   // XXX: split into seperate process
   dns_server.start(); // XXX: need dns_server.stop()
   https_server.start(); // XXX: need https_server.stop()
-  turn_server.start(); // XXX: need turn_server.stop()
   new peer_relay({id, bootstrap: [], port: 3032});
   new peer_relay({id: id2, bootstrap: ['ws://poc.lif.zone:3032'], port: 3033});
 }
