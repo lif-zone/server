8109dc689d9e8fa5970a83dd6af1f8706a74d51f
lif-rnd <lif.zone.main@gmail.com>
2021-12-25 18:27:15 +0200

    rm oboslete

--- peer-relay/test.js
+++ peer-relay/test.js
@@ -13,75 +13,8 @@ import string from '../util/string.js';
 import Node from '../peer-relay/client.js';
 import {EventEmitter} from 'events';
 
-function normalize(o){
-  if (!o.p2 || o.dir!='<')
-    return o;
-  let p = o.p1;
-  o.dir = '>';
-  o.p1 = o.p2;
-  o.p2 = p;
-  return o;
-}
-
-// XXX: mv all test api to test_api.js and add test for it
-/* XXX derry: example of test parsing
-ab>(test go(now 3 send:4))
-{orig: 'ab>....', cmd: 'ab>',
-  arg: [{cmd: 'test', orig:'test'}, {cmd: 'go', orig: 'go(now 3 send:4)',
-  arg: [{cmd: 'now'},{cmd: '3'},{cmd: 'send', arg[{cmd: '4'}]]
-{s: 'a', d: 'b', dir: '>'}
-*/
-
-function parse_expr(expr){
-  // XXX derry: fix parser
-  // parse_cmd_single:
-  // skip WS
-  // if EOS return "ok" but "empty"
-  // eat until: EOS, ws, '(', ':'
-  // if '(' scan until closing ')' while ++ on ( and --  on ).
-  //    stop on 0. Error on >0
-  // if 'cmd' has ':', so validate !args, set args to rest after ':'
-  // OK: if WS or EOS
-  // parse_cmd_multi:
-  // loop on parse_cmd_single()
-  let a = expr.match(/(^[a-zA-Z]{0,2})([<>]+)(.+.*$)/);
-  if (!a || a.length!=4)
-    throw new Error('invalid expr');
-  let {op, params} = parse_cmd(a[3]);
-  return normalize({p1: a[1][0]||'', p2: a[1][1]||'', dir: a[2], op, params});
-}
-
-function parse_param(s){
-  let a = s.split(':'), param = {};
-  if (a.length>2)
-    throw new Error('invalid param');
-  if (!a[0])
-  {
-    if (a.length!=1)
-      throw new Error('invalid param');
-  }
-  else
-    param[a[0]] = a[1]||'';
-  return param;
-}
-
-function parse_params(str){
-  let a = str.split(','), params = {};
-  a.forEach(s=>Object.assign(params, parse_param(s)));
-  return params;
-}
-
-function parse_cmd(cmd){
-  let m = cmd.match(/(^[^(^)]+)(\(([^(^)]*)\))?$/);
-  if (!m || m.length>4)
-    throw new Error('invalid cmd');
-  let op = m[1], rest = m[3]||'', params = parse_params(rest);
-  return {op, params};
-}
-
 function throw_invalid(s, i){
-  throw new Error('invalid '+s.substr(0, i)+'^^^'+s.substr(i));
-}
+  throw new Error('invalid '+s.substr(0, i)+'^^^'+s.substr(i)); }
 
 // XXX: mv all test api to test_api.js and add test for it
 /* XXX derry: example of test parsing
@@ -182,24 +115,8 @@ function test_parse_cmd_multi(s){
   return ret.concat(test_parse_cmd_multi(s.substr(t.meta.last)));
 }
 
-describe('test_api2', function(){
-  it('test_parse_single_invalid', ()=>{
-    const t = (s, exp)=>{ assert.throws(()=>{ test_parse_cmd_single(s); },
-      {message: exp}); };
-    t('abcdefg)12345678', 'invalid abcdefg^^^)12345678');
-    t(')', 'invalid ^^^)');
-    t('(', 'invalid ^^^(');
-    t('a)', 'invalid a^^^)');
-    t('a(b()', 'invalid a(b()^^^');
-    t('a(b () ', 'invalid a(b () ^^^');
-    t('a:(b)', 'invalid a^^^:');
-    t('a:b:c', 'invalid a:b^^^:c');
-    // t('open(a)) ', '');
-    // t('open(a b( c) ', 'xxxx');
-    // t('open(a() ', '');
-    // t('open(a ', {cmd: 'open', arg: 'a'});
-  });
-  it('test_parse_single_valid', ()=>{
+describe('test_api', function(){
+   it('test_parse_cmd_single_valid', ()=>{
     const t = (s, exp, exp_last)=>{
       let ret = test_parse_cmd_single(s);
       let {last} = ret.meta;
@@ -210,7 +127,6 @@ describe('test_api2', function(){
     t('open', {cmd: 'open'}, 4);
     t('open ', {cmd: 'open'}, 5);
     t('open b', {cmd: 'open'}, 5);
-    debugger;
     t('open:a', {cmd: 'open', arg: 'a'}, 6);
     t('open:ab', {cmd: 'open', arg: 'ab'}, 7);
     t('open()', {cmd: 'open'}, 6);
@@ -227,7 +143,19 @@ describe('test_api2', function(){
     t('open(a) )', {cmd: 'open', arg: 'a'}, 7);
     t('bc>(hc hget)', {cmd: 'bc>', arg: 'hc hget'}, 12);
   });
-  it('test_parse', ()=>{
+  it('test_parse_cmd_single_invalid', ()=>{
+    const t = (s, exp)=>{ assert.throws(()=>{ test_parse_cmd_single(s); },
+      {message: exp}); };
+    t('abcdefg)12345678', 'invalid abcdefg^^^)12345678');
+    t(')', 'invalid ^^^)');
+    t('(', 'invalid ^^^(');
+    t('a)', 'invalid a^^^)');
+    t('a(b()', 'invalid a(b()^^^');
+    t('a(b () ', 'invalid a(b () ^^^');
+    t('a:(b)', 'invalid a^^^:');
+    t('a:b:c', 'invalid a:b^^^:c');
+  });
+  it('test_parse_cmd_multi', ()=>{
     const t = (s, exp)=>{
       let ret = test_parse_cmd_multi(s);
       assert.deepEqual(ret, exp);
@@ -245,49 +173,6 @@ describe('test_api2', function(){
     t('ab>(test go(now 3 send:4))', [{cmd: 'ab>', arg: [{cmd: 'test'},
       {cmd: 'go', arg: [{cmd: 'now'}, {cmd: '3'}, {cmd: 'send',
         arg: [{cmd: '4'}]}]}]}]);
-  });
-});
-
-// XXX: rm
-describe('test_api', function(){
-  it('parse_param', ()=>{
-    let t = (s, exp)=>assert.deepEqual(parse_param(s), exp);
-    t('', {});
-    t('ws', {ws: ''});
-    t('ws:80', {ws: '80'});
-  });
-  it('parse_params', ()=>{
-    let t = (s, exp)=>assert.deepEqual(parse_params(s), exp);
-    t('', {});
-    t('ws', {ws: ''});
-    t('ws:80', {ws: '80'});
-    t('ws:80,host:lif.zone', {ws: '80', host: 'lif.zone'});
-  });
-  it('parse_cmd', ()=>{
-    let t = (s, op, params)=>assert.deepEqual(parse_cmd(s), {op, params});
-    t('connect', 'connect', {});
-    t('connect()', 'connect', {});
-    t('connect(ws)', 'connect', {ws: ''});
-    t('connect(ws:80)', 'connect', {ws: '80'});
-    t('connect(ws:80,timeout:5)', 'connect', {ws: '80', timeout: '5'});
-  });
-  it('parse_expr', ()=>{
-    let t = (s, p1, p2, dir, op, params)=>assert.deepEqual(parse_expr(s),
-      {p1, p2, dir, op, params});
-    t('<listen', '', '', '<', 'listen', {});
-    t('a<listen', 'a', '', '<', 'listen', {});
-    t('A<listen', 'A', '', '<', 'listen', {});
-    t('a<listen(ws)', 'a', '', '<', 'listen', {ws: ''});
-    t('a<listen(ws:b)', 'a', '', '<', 'listen', {ws: 'b'});
-    t('a<listen(ws:3030)', 'a', '', '<', 'listen', {ws: '3030'});
-    t('a>new_node(ws:s)', 'a', '', '>', 'new_node', {ws: 's'});
-    t('ab<connect', 'b', 'a', '>', 'connect', {});
-    t('ab>connect(ws:3030)', 'a', 'b', '>', 'connect', {ws: '3030'});
-    t = (s, exp)=>assert.throws(()=>{ parse_expr(s); }, {message: exp});
-    t('', 'invalid expr');
-    t('ab', 'invalid expr');
-    t('ab<', 'invalid expr');
-    t('abc<listen', 'invalid expr');
   });
 });
 
@@ -345,12 +230,14 @@ function on_event(e){
 }
 
 async function run_test(role, test){
+  if (true) return; // XXX: rm obsolete run_test
   let a = string.split_ws(test);
   evil_dns.add('lif.zone', '127.0.0.1');
   for (let i=0; i<a.length; i++)
   {
     let expr = a[i];
-    let {p1, p2, dir, op, params} = parse_expr(expr);
+//    let {p1, p2, dir, op, params} = parse_expr(expr);
+    let {p1, p2, dir, op, params} = {};
     console.log('%s: p1 %s p2 %s dir %s op %s params %s',
       expr, p1, p2, dir, op, params);
     switch (op)
