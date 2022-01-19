87abfe2b7ef8d02467eaa04f4f0468f37d34ce41
lif-rnd <lif.zone.main@gmail.com>
2022-01-19 10:07:21 +0200

    coding

--- peer-relay/test.js
+++ peer-relay/test.js
@@ -654,8 +654,8 @@ describe('peer-relay', function(){
       node(s wss) node(a wss) node(b wss) -
       as>!connect(wss) as>find(a r(s)) as<find(a r(s)) -
       bs>!connect(wss) bs>find(b r(s)) bs<find(s r(s))
-      bs,sa>fwd(ba>conn_info(r(ws)))
-      ba>connect(wss) ba>find(b r(bs)) ba<find(a r(abs))`);
+      bs,sa>fwd(ba>conn_info(r(ws))) ba>connect(wss) ba>find(b r(bs))
+      ba<find(a r(abs))`);
     t = (name, test)=>{
       xit(name, 'a', test);
       xit(name, 'b', test);
