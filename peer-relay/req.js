0de77d765bca0e13b10af5e2a03e86ba222c1cbb
lif-rnd <lif.zone.main@gmail.com>
2022-02-13 18:43:39 +0200

    use etask for req

--- peer-relay/req.js
+++ peer-relay/req.js
@@ -4,6 +4,7 @@ import {EventEmitter} from 'events';
 import assert from 'assert';
 import xerr from '../util/xerr.js';
 import date from '../util/date.js';
+import etask from '../util/etask.js';
 import util from '../util/util.js';
 const REQ_TIMEOUT = 20*date.ms.SEC;
 const b2s = util.buf_to_str;
@@ -20,12 +21,14 @@ function res_handler(body, from, msg){
     return xerr.notice('req not found %s', req_id);
   let req = reqs[req_id];
   delete reqs[req_id];
-  clearTimeout(req.timeout);
+  if (req.timeout)
+    req.timeout.return();
+  if (req.timer)
+    clearTimeout(req.timer);
   req.emit('res', msg);
 }
 
 export default class Req extends EventEmitter {
-  // {node, dst, hdr, body}
   constructor(opt){
     super();
     let {hdr, body, node, dst} = opt;
@@ -35,6 +38,7 @@ export default class Req extends EventEmitter {
     this.node = node;
     let router = this.router = node.router;
     this.dst = dst;
+    this.cmd = cmd;
     let from=b2s(node.id), path=[];
     assert(util.is_mocha() || !req_id, 'manual req_id only in tests '+req_id);
     req_id = req_id || ''+free_req_id++;
@@ -42,14 +46,24 @@ export default class Req extends EventEmitter {
     let msg = {req_id, ts, type: 'req', to: dst, from, nonce, cmd, body, path};
     router._touched[nonce] = true; // XXX: mv out of here
     msg.sign = router.wallet.sign(msg); // XXX: mv out of here
-    // XXX: use etask
-    let timeout = setTimeout(()=>{
-      delete reqs[req_id];
-      this.emit('fail', {error: 'timeout', req_id});
-    }, REQ_TIMEOUT);
     reqs[req_id] = this;
+    this.hdr = hdr;
+    this.body = body;
     this.req_id = req_id; // XXX: change to id
-    this.timeout = timeout;
+    if (true){ // XXX: check why etask doen't work
+      let timer = setTimeout(()=>{
+        delete reqs[req_id];
+        this.emit('fail', {error: 'timeout', req_id});
+      }, REQ_TIMEOUT);
+      this.timer = timer;
+    } else {
+      let timeout = etask({'this': this}, function*req_timeout(){
+        yield etask.sleep(REQ_TIMEOUT);
+        delete reqs[req_id];
+        this.this.emit('fail', {error: 'timeout2', req_id});
+      });
+      this.timeout = timeout;
+    }
     // XXX HACK
     if (util.is_mocha())
       this.test_send = ()=>(router._send(msg), this);
