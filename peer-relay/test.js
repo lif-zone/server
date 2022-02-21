62aa630edce629ed240f4c1e1a2ceb0d10bc2596
lif-rnd <lif.zone.main@gmail.com>
2022-02-21 09:42:53 +0200

    stream test

--- peer-relay/test.js
+++ peer-relay/test.js
@@ -1599,39 +1599,64 @@ describe('peer-relay', function(){
       // managed api/msg
       /*
       client:
-      let req = new Req({node, dst, persistent});
+      let req = new Req({node, dst, stream: true, cmd, body});
       req.on('res_start', (req, res)=>{
-        res.send();
+        xerr.notice('got msg seq % body %s', res.seq, res.body);
+        req.send({body2});
       });
-      req.on('res_next', (req, res)=>{
-      });
-      req.on('res_end', (req, res)=>{
-        // assert on res.send()
-      });
+      req.on('res_next', (req, res)=>{});
+      req.on('res_end', (req, res)=>{});
+      req.on('fail', err=>{});
+
       server:
-      let req_handler = new ReqHandler({node});
+      let req_handler = new ReqHandler({node, cmd});
       req_handler.on('req_start', (req, res)=>{
+        xerr.notice('got msg seq % body %s', req.seq, req.body);
+        res.send({body2});
+        req.on('req_next', (req, res)=>{});
+        req.on('req_end', (req, res)=>{});
+        req.on('fail', err=>{});
       });
       req_handler.on('req_next', (req, res)=>{});
       req_handler.on('req_end', (req, res)=>{});
       if (true)
         return;
-      t('manual', `setup:2_nodes setup(req msg)
-        ab>!req_start(id:r0 body:ping) ab>req_start(id:r0 seq:0 body:ping)
-        ab>msg(type:req_start....)
-        ab<!res_start(id:r0 body:ping) ab<res_start(id:r0 seq:0 body:ping)
+      t('stream', `setup:2_nodes setup:req
+        ab>!req_start(id:r0 stream body:ping)
+        ab>req_start(id:r0 seq:0 body:ping)
+        ab>!res_start(id:r0 body:pong) ab>res_start(id:r0 seq:0 body:pong)
         ab>!req_next(id:r0 body:ping2) ab>req_next(id:r0 seq:1 body:ping2)
-        ab<!res_next(id:r0 body:pong2) ab<res_next(id:r0 seq:1 body:pong2)
-        ab>!req_end(id:r0 body:ping3) ab>req_end(id:r0 seq:3 body:ping3)
-        ab<!res_end(id:r0 body:pong3) ab<res_end(id:r0 seq:3 body:pong3)
+        ab>!res_next(id:r0 body:pong2) ab>res_next(id:r0 seq:1 body:pong2)
+        ab>!req_end(id:r0 body:ping3) ab>req_end(id:r0 seq:2 body:ping3)
+        ab>!res_end(id:r0 body:pong3) ab>res_end(id:r0 seq:2 body:pong3)
       `);
-      t('auto', `setup:2_nodes
-        ab>!req_start(id:r0 body:ping res:pong))
-        ab>req_start(id:r0 seq:0 body:ping) ab<res_start(id:r0 seq:0 body:ping)
-        ab>!req_next(id:r0 body:ping2 res:pong2)
-        ab>req_next(id:r0 seq:1 body:ping2) ab<res_next(id:r0 seq:1 body:pong2)
-        ab>!req_end(id:r0 body:ping3 res:pong3)
-        ab>req_end(id:r0 seq:3 body:ping3) ab<res_end(id:r0 seq:3 body:pong3)
+      t('stream2', `setup:2_nodes setup:req
+        ab>!req_start(id:r0 stream body:ping)
+        ab>req_start(id:r0 seq:0 body:ping)
+        ab>!res_start(id:r0 body:pong) ab>res_start(id:r0 seq:0 body:pong)
+        ab>!res_next(id:r0 body:pong2) ab>res_next(id:r0 seq:1 body:pong2)
+        ab>!res_end(id:r0 body:pong3) ab>res_end(id:r0 seq:2 body:pong3)
+      `);
+      t('stream3', `setup:2_nodes setup(req msg)
+        ab>!req_start(id:r0 stream body:ping)
+        ab>msg(type:req_start id:r0 seq:0 body:ping)
+        ab>req_start(id:r0 seq:0 body:ping)
+        ab>!msg(type:res_start id:r0 seq:0 body:pong)
+        ab>msg(type:res_start id:r0 seq:0 body:pong)
+        ab>res_start(id:r0 seq:0 body:pong)
+        ...
+      `);
+      t('stream-invalid order', `setup:2_nodes setup(req msg)
+        ab>!req_start(id:r0 stream body:ping)
+        ab>msg(type:req_start id:r0 seq:0 body:ping)
+        ab>req_start(id:r0 seq:0 body:ping)
+        ab>!msg(type:res_next id:r0 seq:1 body:pong2)
+        ab>msg(type:res_next id:r0 seq:1 body:pong2)
+        ab>!msg(type:res_start id:r0 seq:0 body:pong)
+        ab>msg(type:res_start id:r0 seq:0 body:pong)
+        ab>res_start(id:r0 seq:0 body:pong)
+        ab>res_next(id:r0 seq:1 body:pong2)
+        ...
       `);
       */
     });
