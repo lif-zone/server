0894abe811893512fa1ed8a40bd9e840049251ed
lif-rnd <lif.zone.main@gmail.com>
2022-03-30 15:03:13 +0300

    swarm demo

--- storage/demo_swarm.js
+++ storage/demo_swarm.js
@@ -3,6 +3,23 @@ import Swarm from 'hyperswarm';
 start();
 
 async function start(){
+  const [seed] = process.argv.slice(2);
+  if (seed){
+    console.log('running as swarm demo');
+    const swarm1 = new Swarm({seed: Buffer.alloc(32).fill(seed)});
+    swarm1.on('connection', function(connection, info){
+      console.log('swarm 1 got a server connection:',
+        connection.remotePublicKey, connection.publicKey,
+        connection.handshakeHash);
+      connection.on('error', err => console.error('1 CONN ERR:', err));
+    });
+    console.log('SWARM 1 KEYPAIR:', swarm1.keyPair);
+    const key = Buffer.alloc(32).fill(7);
+    const discovery1 = swarm1.join(key);
+    await discovery1.flushed();
+    return;
+  }
+  console.log('running as standalone swarm demo');
   const swarm1 = new Swarm({seed: Buffer.alloc(32).fill(4)});
   const swarm2 = new Swarm({seed: Buffer.alloc(32).fill(5)});
 
