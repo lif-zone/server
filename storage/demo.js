6e890e6f5888b45d553f5f2283346fe31440d52e
lif-rnd <lif.zone.main@gmail.com>
2022-03-30 15:23:32 +0300

    demo

--- storage/demo.js
+++ storage/demo.js
@@ -1,5 +1,4 @@
 // author: derry. coder: arik.
-import net from 'net';
 import hypercore from 'hypercore';
 import Swarm from 'hyperswarm';
 
@@ -9,7 +8,6 @@ async function start(){
   if (['client', 'server'].indexOf(mode)==-1 || !port)
     exit('usage: node index.js [client|server] [PORT] [KEY]');
   const is_client = mode=='client';
-  const hostname = 'peer1.lif.zone';
   const feed = hypercore(is_client ?
     '/tmp/demo_client' : '/tmp/demo_server', key, {valueEncoding: 'utf8'});
   feed.ready(()=>{
@@ -38,6 +36,7 @@ async function start(){
     console.log('swarm 1 got a server connection:',
       connection.remotePublicKey, connection.publicKey,
       connection.handshakeHash);
+    onconnection(connection, !is_client);
     connection.on('error', err => console.error('1 CONN ERR:', err));
   });
   console.log('SWARM 1 KEYPAIR:', swarm1.keyPair);
@@ -45,6 +44,22 @@ async function start(){
   const discovery1 = swarm1.join(topic);
   await discovery1.flushed();
 
+  function onconnection(socket, isInitiator){
+    const remoteAddr = `${socket.remoteAddress}:${socket.remotePort}`;
+    console.log(`new connection from ${remoteAddr}`);
+    console.log(`starting to replicate`, feed);
+    const proto = feed.replicate(isInitiator, {live: true, noise: false});
+    feed.on('error', err=>console.error('feed: error %o', err));
+    feed.on('download', ()=>console.log('feed: download len %s', feed.length));
+    feed.on('upload', ()=>console.log('feed: upload'));
+    feed.on('append', ()=>console.log('feed: append'));
+    feed.on('sync', ()=>console.log('feed: sync'));
+    feed.on('close', ()=>console.log('feed: close'));
+    feed.on('ready', ()=>console.log('feed: ready'));
+    proto.pipe(socket).pipe(proto);
+    socket.on('close',
+      ()=>console.log(`connection closed from ${remoteAddr}`));
+  }
 }
 
 function exit(msg){
