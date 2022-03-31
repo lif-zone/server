30a815ccf2e02b95122b8bbfcf827263210e6568
lif-rnd <lif.zone.main@gmail.com>
2022-03-31 10:20:25 +0300

    demo node

--- peer-relay/demo.js
+++ peer-relay/demo.js
@@ -1,13 +1,25 @@
 // author: derry. coder: arik.
 import Node from './client.js';
 import {LocalStorage} from 'node-localstorage';
-
+import util from '../util/util.js';
+const s2b = util.buf_from_str, b2s = util.buf_to_str;
 const localStorage = new LocalStorage('/var/lif/demo_local_storage');
 
 start();
 async function start(){
-  console.log('XXX load %s', localStorage.getItem('xxx'));
   localStorage.setItem('xxx', ''+Date.now());
-  console.log('XXX new %s', localStorage.getItem('xxx'));
+  let priv = localStorage.getItem('wallet_key_priv');
+  let pub = localStorage.getItem('wallet_key_pub');
+  if (!priv || !pub){
+    priv = pub = undefined;
+    console.log('XXX no keys found - generating new keys');
+  }
+  let node = new Node({keys: {priv: s2b(priv), pub: s2b(pub)}});
+  priv = b2s(node.wallet.keys.priv);
+  pub = b2s(node.wallet.keys.pub);
+  localStorage.setItem('wallet_key_priv', priv);
+  localStorage.setItem('wallet_key_pub', pub);
+  console.log('XXX node priv %s', b2s(node.wallet.keys.priv));
+  console.log('XXX node pub %s', b2s(node.wallet.keys.pub));
 }
 
