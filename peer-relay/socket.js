790597ab5c9c2df0780364cdac3810d2e40202a4
lif-rnd <lif.zone.main@gmail.com>
2022-04-07 09:40:38 +0300

    coding

--- peer-relay/socket.js
+++ peer-relay/socket.js
@@ -1,73 +1,70 @@
 // author: derry. coder: arik.
-/* eslint-disable */ // XXX: fix and rm
-'use strict'
+'use strict'; /*jslint node:true, browser:true*/
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('util').inherits;
+var Node = require('./node');
+module.exports = Socket;
 
-var EventEmitter = require('events').EventEmitter
-var inherits = require('util').inherits
-var Node = require('./node')
+var PORT_MAPPINGS = {};
+var BIND_STATE_UNBOUND = 0;
+var BIND_STATE_BINDING = 1;
+var BIND_STATE_BOUND = 2;
 
-module.exports = Socket
+inherits(Socket, EventEmitter);
+function Socket(opts){
+  if (!(this instanceof Socket))
+    return new Socket(opts);
 
-var PORT_MAPPINGS = {}
+  var self = this;
+  self._port = Math.floor(Math.random() * 9999);
+  self._bindState = BIND_STATE_UNBOUND;
+  self.peer = new Node(opts);
+  self.peer.on('message', onMessage);
+  self.peer.on('close', onClose);
+  self.peer.on('error', onError);
+  PORT_MAPPINGS[self._port] = self.peer.id.toString('hex');
 
-var BIND_STATE_UNBOUND = 0
-var BIND_STATE_BINDING = 1
-var BIND_STATE_BOUND = 2
-
-inherits(Socket, EventEmitter)
-function Socket (opts) {
-  if (!(this instanceof Socket)) return new Socket(opts)
-
-  var self = this
-  self._port = Math.floor(Math.random() * 9999)
-  self._bindState = BIND_STATE_UNBOUND
-  self.peer = new Node(opts)
-  self.peer.on('message', onMessage)
-  self.peer.on('close', onClose)
-  self.peer.on('error', onError)
-
-  PORT_MAPPINGS[self._port] = self.peer.id.toString('hex')
-
-  function onMessage (msg) {
+  function onMessage(msg){
     var rinfo = {
       address: msg.address,
       port: msg.port,
       family: 'peer-relay'
-    }
-    self.emit('message', new Buffer(msg.buffer), rinfo)
+    };
+    self.emit('message', new Buffer(msg.buffer), rinfo);
   }
 
-  function onClose () {
-    self.close()
-  }
+  function onClose(){ self.close(); }
 
-  function onError (err) {
-    self.emit('error', err)
+  function onError(err){
+    self.emit('error', err);
   }
 }
 
-Socket.prototype.bind = function (port, cb) {
-  var self = this
-  if (!self.peer) throw new Error('Not running')
-  if (self._bindState !== BIND_STATE_UNBOUND) throw new Error('Socket is already bound')
+Socket.prototype.bind = function(port, cb){
+  var self = this;
+  if (!self.peer)
+    throw new Error('Not running');
+  if (self._bindState !== BIND_STATE_UNBOUND)
+    throw new Error('Socket is already bound');
 
-  delete PORT_MAPPINGS[self._port]
-  self._port = port || Math.floor(Math.random() * 9999)
-  self._bindState = BIND_STATE_BINDING
-  if (cb) self.once('listening', cb)
+  delete PORT_MAPPINGS[self._port];
+  self._port = port || Math.floor(Math.random() * 9999);
+  self._bindState = BIND_STATE_BINDING;
+  if (cb)
+    self.once('listening', cb);
 
-  if (self.peer.peers.count() > 0) {
-    onBind()
+  if (self.peer.peers.count() > 0){
+    onBind();
   } else {
-    self.peer.once('peer', onBind)
+    self.peer.once('peer', onBind);
   }
 
-  function onBind () {
-    PORT_MAPPINGS[self._port] = self.peer.id.toString('hex')
-    self._bindState = BIND_STATE_BOUND
-    self.emit('listening')
+  function onBind(){
+    PORT_MAPPINGS[self._port] = self.peer.id.toString('hex');
+    self._bindState = BIND_STATE_BOUND;
+    self.emit('listening');
   }
-}
+};
 
 // valid combinations
 // send(buffer, offset, length, port, address, cb)
@@ -76,78 +73,84 @@ Socket.prototype.bind = function (port, cb) {
 // send(bufferOrList, port, address, cb)
 // send(bufferOrList, port, address)
 // send(bufferOrList, port)
-Socket.prototype.send = function (buffer, offset, length, port, address, cb) {
-  var self = this
-  if (!self.peer) throw new Error('Not running')
+Socket.prototype.send = function(buffer, offset, length, port, address, cb){
+  var self = this;
+  if (!self.peer)
+    throw new Error('Not running');
 
-  var isIP = address.indexOf('.') !== -1
-  var isLocal = address === 'localhost' || address === '127.0.0.1'
+  var isIP = address.indexOf('.') !== -1;
+  var isLocal = address === 'localhost' || address === '127.0.0.1';
 
-  var id = isLocal ? PORT_MAPPINGS[port] : address
+  var id = isLocal ? PORT_MAPPINGS[port] : address;
   var msg = {
     buffer: buffer.slice(offset, length),
     address: isLocal ? address : self.peer.id.toString('hex'),
     port: self._port
-  }
+  };
 
-  if (id == null || (isIP && !isLocal)) return
+  if (id == null || isIP && !isLocal)
+    return;
 
-  self.peer.send(new Buffer(id, 'hex'), msg)
-  if (cb) cb()
-}
+  self.peer.send(new Buffer(id, 'hex'), msg);
+  if (cb)
+    cb();
+};
 
-Socket.prototype.close = function (cb) {
-  var self = this
-  if (!self.peer) throw new Error('Not running')
+Socket.prototype.close = function(cb){
+  var self = this;
+  if (!self.peer)
+    throw new Error('Not running');
 
-  self.peer.destroy()
-  self.peer = null
+  self.peer.destroy();
+  self.peer = null;
 
-  delete PORT_MAPPINGS[self._port]
+  delete PORT_MAPPINGS[self._port];
 
-  if (cb) self.on('close', cb)
-  self.emit('close')
-}
+  if (cb)
+    self.on('close', cb);
+  self.emit('close');
+};
 
-Socket.prototype.address = function () {
-  var self = this
-  if (!self.peer) throw new Error('Not running')
+Socket.prototype.address = function(){
+  var self = this;
+  if (!self.peer)
+    throw new Error('Not running');
 
   return {
     address: self.peer.id.toString('hex'),
     port: self._port,
     family: 'peer-relay'
-  }
-}
+  };
+};
 
-Socket.prototype.setBroadcast = function () {
-  throw new Error('setBroadcast not implemented')
-}
+Socket.prototype.setBroadcast = function(){
+  throw new Error('setBroadcast not implemented');
+};
 
-Socket.prototype.setTTL = function () {
-  throw new Error('setTTL not implemented')
-}
+Socket.prototype.setTTL = function(){
+  throw new Error('setTTL not implemented');
+};
 
-Socket.prototype.setMulticastTTL = function () {
-  throw new Error('setMulticastTTL not implemented')
-}
+Socket.prototype.setMulticastTTL = function(){
+  throw new Error('setMulticastTTL not implemented');
+};
 
-Socket.prototype.setMulticastLoopback = function () {
-  throw new Error('setMulticastLoopback not implemented')
-}
+Socket.prototype.setMulticastLoopback = function(){
+  throw new Error('setMulticastLoopback not implemented');
+};
 
-Socket.prototype.addMembership = function () {
-  throw new Error('addMembership not implemented')
-}
+Socket.prototype.addMembership = function(){
+  throw new Error('addMembership not implemented');
+};
 
-Socket.prototype.dropMembership = function () {
-  throw new Error('dropMembership not implemented')
-}
+Socket.prototype.dropMembership = function(){
+  throw new Error('dropMembership not implemented');
+};
 
-Socket.prototype.ref = function () {
-  throw new Error('ref not implemented')
-}
+Socket.prototype.ref = function(){
+  throw new Error('ref not implemented');
+};
 
-Socket.prototype.unref = function () {
-  throw new Error('unref not implemented')
-}
+Socket.prototype.unref = function(){
+  throw new Error('unref not implemented');
+};
