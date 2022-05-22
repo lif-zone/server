54074854e63adfed741f68307880532413c753fb
lif-rnd <lif.zone.main@gmail.com>
2022-05-22 09:17:26 +0300

    node_conn

---
+++ peer-relay/node_conn.js
@@ -0,0 +1,9 @@
+// author: derry. coder: arik.
+'use strict'; /*jslint node:true, browser:true*/
+import {EventEmitter} from 'events';
+
+export default class NodeConn extends EventEmitter {
+constructor(){
+  super();
+}
+}
