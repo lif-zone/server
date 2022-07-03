ed27eaf9b19a41c6db77e7987b185173acc8c2d8
lif-rnd <lif.zone.main@gmail.com>
2022-07-03 12:24:07 +0300

    is_lower/is_upper

--- util/string.js
+++ util/string.js
@@ -9,3 +9,5 @@ E.split_trim = function(s, sep, limit){
 E.split_ws = function(s){ return E.split_trim(s, /\s+/); };
 E.is_ws = function(s){ return /^\s$/.test(s); };
 E.sort_char = s=>s.split('').sort((a, b)=>a>b ? -1 : a<b ? 1 : 0).join('');
+E.is_lower = function(ch){ return /^[a-z]$/.test(ch); };
+E.is_upper = function(ch){ return /^[A-Z]$/.test(ch); };
